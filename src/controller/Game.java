8ec4f6f0608791a1bcdd17e5f1f1bafc424010b7
Manuel <m-allenspach@hotmail.com>
2014-09-04 11:13:33 +0200

    Update Game.java
    Creates deepcopy of board before passing it to players, win-condition was wrong, ...

--- src/controller/Game.java
+++ src/controller/Game.java
@@ -10,7 +10,7 @@ public class Game {
 	
 	public Game(Player player1, Player player2) {
 		board = new Board();
-		board.initializePieces();
+		board.initialize();
 		players[0] = player1;
 		players[0].setTeam(Color.WHITE);
 		players[1] = player2;
@@ -23,24 +23,20 @@ public class Game {
 			if (!turnAvailable(players[i])) {
 				draw = true;
 			} else {
-				long start = System.currentTimeMillis();
-				makeTurn(players[i]);
-				if ((System.currentTimeMillis() - start) > MAX_MILLISECONDS) {
-					players[i].setDisqualified();
-				}
+				makeTurn(players[i], players[(i+1) % 2]);
 				i = (i + 1) % 2;
 			}
 		}
-		if (wins(players[0]) && !wins(players[1])) {
-			return Controller.WIN_POINTS;
-		} else if (wins(players[1]) && !wins(players[0])) {
+		if (loses(players[0]) && !loses(players[1])) {
 			return Controller.LOSE_POINTS;
+		} else if (loses(players[1]) && !loses(players[0])) {
+			return Controller.WIN_POINTS;
 		} else {
 			return Controller.DRAW_POINTS;
 		}
 	}
 	
-	private boolean wins(Player player) {
+	private boolean loses(Player player) {
 		if (player.isDisqualified() || board.getKing(player) == null) {
 			return true;
 		}
@@ -49,49 +45,38 @@ public class Game {
 	
 	// player can make a turn
 	private boolean turnAvailable(Player player) {
-		for (Piece piece : player.getPieces(board.getOriginalFields())) {
-			if (piece.getValidDestinationSet(board.getOriginalFields()).size() > 0) {
+		for (Piece piece : player.getPieces(board)) {
+			if (piece.getValidDestinationSet(board).size() > 0) {
 				return true;
 			}
 		}
 		return false;
 	}
 	
-	private void makeTurn(Player player) {
-		player.setCheck(isCheck(player));
+	private void makeTurn(Player player, Player enemy) {
+		player.setCheck(board.isCheck(player, enemy));
+		enemy.setCheck(board.isCheck(enemy, player));
 		try {
-			Move move = player.getMove(board.getFields());
-			if (move.isValid(board.getOriginalFields(), player)) {
+			long start = System.currentTimeMillis();
+			
+			Move move = player.getMove(board.copy(), enemy);
+			if ((System.currentTimeMillis() - start) > MAX_MILLISECONDS) {
+				player.setDisqualified();
+			}
+			if (move.isValid(board, player)) {
 				if (board.movePiece(move) || move.getPiece().getType() == PieceType.PAWN) {
 					turnsWithoutCaptures = 0;
 				} else {
 					turnsWithoutCaptures++;
 				}
+			} else {
+				player.setDisqualified(); //invalid move
 			}
 		} catch (Exception e) {
 			player.setDisqualified();
+			System.out.println("Exception while moving " + player);
 		}
 		
-	}
-	
-	public boolean isCheck(Player player) {
-		Piece king = board.getKing(player);
-		Point pos = king.getPos();
-		Color enemy = king.getTeam().opposite();
-		Field[][] fields = board.getFields();
-		
-		for (Field[] rows : board.getOriginalFields()) {
-			for (Field field : rows) {
-				if (field.hasPiece()) {
-					Piece piece = field.getPiece();
-					if (piece.getTeam() == enemy && 
-							piece.getValidDestinationSet(fields).contains(pos)){
-						return true;
-					}
-				}
-			}
-		}
-		return false;
 	}
 
 	public boolean gameOver() {
